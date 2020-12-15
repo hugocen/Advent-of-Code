1eeb57ed0a147bc2b8bd9c2981cd62fcfa916b09
Hugo Chen <hugo.chen@telexpress.com>
2020-12-15 14:09:23 +0800

    2020 day 14

--- 2020/Day14/src/main.rs
+++ 2020/Day14/src/main.rs
@@ -1,8 +1,5 @@
 use std::fs;
 use std::collections::HashMap;
-use itertools::Itertools;
-use std::thread;
-use std::sync::{Arc, Mutex};
 
 fn main() {
     let contents = fs::read_to_string("inputs.txt").expect("Error");
@@ -15,12 +12,10 @@ fn main() {
         let sen: Vec<&str> = line.split(" = ").collect();
         
         if sen[0] == "mask" {
-            // println!("{}", &sen[1]);
             let new_mask = format!("{:X>64}", &sen[1]);
             let new_mask2 = format!("{:0>64}", &sen[1]);
             mask = new_mask.to_owned();
             mask2 = new_mask2.to_owned();
-            println!("{}", mask2);
         } else {
             let idx = sen[0][4..sen[0].len()-1].parse::<u64>().expect("parse error");
             let value = sen[1].parse::<u64>().expect("parse error");
@@ -49,7 +44,6 @@ fn bitmask(mask: String, value: u64) -> u64 {
     let mask_string: Vec<char> = mask.chars().collect();
     let val_bits = format!("{:b}", value);
     let mut val_string: Vec<char> = format!("{:0>64}", val_bits).chars().collect();
-    // println!("{}", mask);
     for i in 0..64 {
         if mask_string[i] != 'X' {
             val_string[i] = mask_string[i];
@@ -57,57 +51,47 @@ fn bitmask(mask: String, value: u64) -> u64 {
     }
     let val_masked: String = val_string.into_iter().collect();
     let m_value = u64::from_str_radix(&val_masked, 2).unwrap();
-    // println!("{}", m_value);
     return m_value;
 }
 
 fn address_mask(mask: String, address: u64) -> Vec<u64> {
-    // let mut addresses = Vec::new();
-    let mut addresses = Arc::new(Mutex::new(vec![]));
     let mask_string: Vec<char> = mask.chars().collect();
     let address_bits = format!("{:b}", address);
     let mut address_string: Vec<char> = format!("{:0>64}", address_bits).chars().collect();
 
-    let mut floating = 0;
-
     for i in 0..64 {
         match mask_string[i] {
-            'X' => {
-                address_string[i] = 'X';
-                floating += 1
-            },
+            'X' => address_string[i] = 'X',
             '0' => {},
             '1' => address_string[i] = '1',
             _ => panic!("error"),
         }
     }
-    let mut nums = vec!['1'; floating];
-    nums.resize(floating*2, '0');
-    // let perms = nums.iter().permutations(floating);
-    let handle =thread::spawn({
-        let clone = Arc::clone(&addresses);
-        move || {
-        let perms = nums.iter().permutations(floating);
-        for perm in perms {
-        let mut new_address = address_string.clone();
-        let mut p = 0;
-        for i in 0..64 {
-            if new_address[i] == 'X' {
-                new_address[i] = perm[p].clone();
-                p += 1;
-            }
+    return recur_address(address_string, 0);
+}
+
+fn recur_address(address: Vec<char>, idx: usize) -> Vec<u64> {
+    let mut addresses = Vec::new();
+    let mut flag = true;
+    for i in idx..address.len() {
+        if address[i] == 'X' {
+            let mut new_address1 = address.clone();
+            new_address1[i] = '1';
+            let mut r1 = recur_address(new_address1, i+1);
+            addresses.append(&mut r1);
+
+            let mut new_address2 = address.clone();
+            new_address2[i] = '0';
+            let mut r2 = recur_address(new_address2, i+1);
+            addresses.append(&mut r2);
+            flag = false;
+            break;
         }
-        let addresses_masked: String = new_address.into_iter().collect();
-        // println!("{}", addresses_masked);
+    }
+    if flag {
+        let addresses_masked: String = address.into_iter().collect();
         let addresses_value = u64::from_str_radix(&addresses_masked, 2).unwrap();
-        let mut v = clone.lock().unwrap();
-        v.push(addresses_value);
-    }}});
-    // println!("");
-
-    handle.join().unwrap();
-
-    // return (addresses.lock().unwrap()).unwrap();
-    return Arc::try_unwrap(addresses).unwrap().into_inner().unwrap();
-
+        addresses.push(addresses_value);
+    }
+    return addresses;
 }
